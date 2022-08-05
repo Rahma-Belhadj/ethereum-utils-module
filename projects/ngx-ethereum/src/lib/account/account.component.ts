7f380ac68f567169e408598a77acabeee93782a0
Rahma BEL HADJ <rahma.belhadj@neofacto.com>
2022-08-05 15:15:48 +0200

    Add account Component into the library

---
+++ projects/ngx-ethereum/src/lib/account/account.component.ts
@@ -0,0 +1,27 @@
+import { Component, Input, ViewChild } from '@angular/core';
+import { MatTooltip } from '@angular/material/tooltip';
+
+@Component({
+  selector: 'lib-ngx-ethereum',
+  templateUrl: './account.component.html',
+  styleUrls: ['./account.component.scss'],
+})
+export class AccountComponent {
+  @Input()
+  accountAddress!: string;
+
+  @ViewChild(MatTooltip)
+  tooltip!: MatTooltip;
+
+  message = 'Copy the address';
+
+  async onCopyToClipboard() {
+    await navigator.clipboard.writeText(this.accountAddress).then(() => {
+      this.tooltip.show();
+      this.message = 'copied !';
+      setTimeout(() => {
+        this.message = 'Copy the address';
+      }, 3000);
+    });
+  }
+}
--- projects/ngx-ethereum/src/lib/ngx-ethereum.module.ts
+++ projects/ngx-ethereum/src/lib/ngx-ethereum.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
+import { MatTooltipModule } from '@angular/material/tooltip';
+import { AccountComponent } from './account/account.component';
 import { NgxEthereumComponent } from './ngx-ethereum.component';
 import { ShortAddressPipe } from './pipes/short-address.pipe';
 
 @NgModule({
-  declarations: [NgxEthereumComponent, ShortAddressPipe],
-  imports: [],
+  declarations: [NgxEthereumComponent, ShortAddressPipe, AccountComponent],
+  imports: [MatTooltipModule],
   exports: [NgxEthereumComponent],
 })
 export class NgxEthereumModule {}
